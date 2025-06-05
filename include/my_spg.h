4210f0dd97c2d5c78fda6b716b72060848ddacba
Michou <michou@epitech.eu>
2025-06-05 15:07:10 +0100

    n°1: first push

---
+++ include/my_spg.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** my_rpg.h
+** File description:
+** just a file
+*/
+
+#ifndef MY_RPG
+   #include <SFML/Graphics.h>
+   #include <sys/stat.h>
+   #include <string.h>
+   #include <fcntl.h>
+   #include <unistd.h>
+   #include <stdlib.h>
+   #include <stdio.h>
+   #include <stdbool.h>
+   #include <math.h>
+   #define MY_RPG
+#endif
